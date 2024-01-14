ca0d80421ab03b71ae89adacff11910e8264cb30
jiemojiemo <460706836@qq.com>
2024-01-14 20:24:33 +0800

    update CodecCapabilitiesTest.kt

--- app/src/androidTest/java/com/example/learnmediacodec/CodecCapabilitiesTest.kt
+++ app/src/androidTest/java/com/example/learnmediacodec/CodecCapabilitiesTest.kt
@@ -1,13 +1,9 @@
 package com.example.learnmediacodec
 
-import android.media.MediaCodecInfo
-import android.media.MediaCodecList
-import android.media.MediaFormat
-import android.os.Environment
+import android.media.*
 import android.util.Log
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
-import com.google.gson.Gson
 import org.junit.Assert.*
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -78,25 +74,27 @@ class CodecCapabilitiesTest {
     }
 
     @Test
-    fun canCreateFromProfileAndLevel(){
+    fun canCreateFromProfileAndLevel() {
         val profile = MediaCodecInfo.CodecProfileLevel.AVCProfileBaseline
         val level = MediaCodecInfo.CodecProfileLevel.AVCLevel31
-        val cap = MediaCodecInfo.CodecCapabilities.createFromProfileLevel("video/avc", profile, level)
+        val cap =
+            MediaCodecInfo.CodecCapabilities.createFromProfileLevel("video/avc", profile, level)
 
         assertNotNull(cap)
     }
 
     @Test
-    fun createFailedIfProfileAndLevelNotUnderstoodByFramework(){
+    fun createFailedIfProfileAndLevelNotUnderstoodByFramework() {
         val profile = 100
         val level = MediaCodecInfo.CodecProfileLevel.H263Level70
-        val cap = MediaCodecInfo.CodecCapabilities.createFromProfileLevel("video/avc", profile, level)
+        val cap =
+            MediaCodecInfo.CodecCapabilities.createFromProfileLevel("video/avc", profile, level)
 
         assertNull(cap)
     }
 
     @Test
-    fun canCheckIsFeatureSupportedOrNot(){
+    fun canCheckIsFeatureSupportedOrNot() {
         val decoderCal = mAVCDecoderInfo?.getCapabilitiesForType(mAVCMime)
 
         assertTrue(decoderCal?.isFeatureSupported(MediaCodecInfo.CodecCapabilities.FEATURE_AdaptivePlayback)!!)
@@ -104,143 +102,65 @@ class CodecCapabilitiesTest {
     }
 
     @Test
-    fun canGetFeaturesFromDefaultFormat(){
+    fun canGetFeaturesFromDefaultFormat() {
         val decoderCal = mAVCDecoderInfo?.getCapabilitiesForType(mAVCMime)
         val defaultFormat = decoderCal?.defaultFormat
 
-        for(f in defaultFormat?.features!!){
+        for (f in defaultFormat?.features!!) {
             assertTrue(decoderCal.isFeatureSupported(f))
         }
     }
 
     @Test
-    fun canGetVideoCapabilities(){
+    fun canGetVideoCapabilities() {
         val decoderCal = mAVCDecoderInfo?.getCapabilitiesForType(mAVCMime)
 
         assertNotNull(decoderCal?.videoCapabilities)
     }
 
     @Test
-    fun getVideoCapabilitiesFailedIfNotVideoCodec(){
+    fun getVideoCapabilitiesFailedIfNotVideoCodec() {
         val decoderCal = mAudioDecoderInfo?.getCapabilitiesForType(mAudioMime)
 
         assertNull(decoderCal?.videoCapabilities)
     }
 
     @Test
-    fun canGetAudioCapabilities(){
+    fun canGetAudioCapabilities() {
         val decoderCal = mAudioDecoderInfo?.getCapabilitiesForType(mAudioMime)
 
         assertNotNull(decoderCal?.audioCapabilities)
     }
 
     @Test
-    fun getAudioCapabilitiesFailedIfNotAudioCodec(){
+    fun getAudioCapabilitiesFailedIfNotAudioCodec() {
         val decoderCal = mAVCDecoderInfo?.getCapabilitiesForType(mAVCMime)
 
         assertNull(decoderCal?.audioCapabilities)
     }
 
     @Test
-    fun canGetEncoderCapabilities(){
+    fun canGetEncoderCapabilities() {
         val encoderCal = mAVCEncoderInfo?.getCapabilitiesForType(mAVCMime)
 
         assertNotNull(encoderCal?.encoderCapabilities)
     }
 
     @Test
-    fun getEncoderCapabilitiesFailedIfNotEncoder(){
+    fun getEncoderCapabilitiesFailedIfNotEncoder() {
         val decoderCal = mAVCDecoderInfo?.getCapabilitiesForType(mAVCMime)
 
         assertNull(decoderCal?.encoderCapabilities)
     }
 
     @Test
-    fun canCheckIsFormatSupportedOrNot(){
+    fun canCheckIsFormatSupportedOrNot() {
         val decoderCal = mAVCDecoderInfo?.getCapabilitiesForType(mAVCMime)
         val encoderCal = mAVCEncoderInfo?.getCapabilitiesForType(mAVCMime)
 
         assertTrue(decoderCal?.isFormatSupported(mAVCFormat)!!)
         assertTrue(encoderCal?.isFormatSupported(mAVCFormat)!!)
     }
-
-    private fun buildProfileLevelsString(profileLevels: Array<MediaCodecInfo.CodecProfileLevel>): String {
-        val sb = StringBuilder()
-        sb.append("supported profile levels:\n")
-        for (profileLevel in profileLevels) {
-            val profileHex = String.format("0x%X", profileLevel.profile)
-            val levelHex = String.format("0x%X", profileLevel.level)
-
-            sb.append("profile: ${profileHex}, level: ${levelHex}\n")
-        }
-
-        return sb.toString()
-    }
-
-    private fun buildColorFormatsString(colorFormats: IntArray): String {
-        val sb = StringBuilder()
-        sb.append("supported color formats:")
-        for (colorFormat in colorFormats) {
-            for (field in capabilitiesFields) {
-                if (field.type == Int::class.java && field.getInt(null) == colorFormat) {
-                    sb.append("${field.name},")
-                    break
-                }
-            }
-        }
-
-        sb.append("\n")
-        return sb.toString()
-    }
-
-    private fun buildVideoCapabilitiesString(vc: MediaCodecInfo.VideoCapabilities?): String {
-        val sb = StringBuilder()
-        sb.append("video capabilities:\n")
-        if(vc == null){
-            return sb.toString()
-        }
-
-        sb.append("supported width range: ${vc.supportedWidths}\n")
-        sb.append("supported height range: ${vc.supportedHeights}\n")
-        sb.append("supported frame rate range: ${vc.supportedFrameRates}\n")
-        sb.append("supported bit rate range: ${vc.bitrateRange}\n")
-        return sb.toString()
-    }
-
-    private fun buildAudioCapabilitiesString(ac: MediaCodecInfo.AudioCapabilities?): String {
-        val sb = StringBuilder()
-        sb.append("audio capabilities:\n")
-        if(ac == null){
-            return sb.toString()
-        }
-
-        sb.append("supported sample rates: ${ac.supportedSampleRates}\n")
-        sb.append("supported channel rate ranges: ${ac.supportedSampleRateRanges}\n")
-        sb.append("supported input channel ranges: ${ac.inputChannelCountRanges}\n")
-        sb.append("supported min input channel: ${ac.minInputChannelCount}\n")
-        sb.append("supported max input channel: ${ac.maxInputChannelCount}\n")
-        sb.append("supported bit rate range: ${ac.bitrateRange}\n")
-        return sb.toString()
-    }
-
-    private fun buildEncoderCapabilitiesString(ec: MediaCodecInfo.EncoderCapabilities?): String {
-        val sb = StringBuilder()
-        sb.append("encoder capabilities:\n")
-        if(ec == null){
-            return sb.toString()
-        }
-
-        sb.append("supported complexity range: ${ec.complexityRange}\n")
-        sb.append("supported quality range: ${ec.qualityRange}\n")
-        return sb.toString()
-    }
-
-    private fun buildCapabilitiesString(cal: MediaCodecInfo.CodecCapabilities): String {
-        return MediaClassJsonUtils.toJson(cal).toString()
-    }
-
-
-
 
     @Test
     fun printAllCapabilities() {
@@ -251,5 +171,59 @@ class CodecCapabilitiesTest {
         val cacheFile = File(cacheDir, "capabilities.json")
         Log.d(TAG, "printAllCapabilities: ${cacheFile.absolutePath}")
         cacheFile.writeText(str)
+    }
+
+    @Test
+    fun createMediaCodecDecoderVideo() {
+        val mime = "video/avc"
+        val width = 3840
+        val height = 2160
+        val videoFormat = MediaFormat.createVideoFormat(mime, width, height)
+
+        // find decoder name
+        val decoderCodecName = mRegularCodes.findDecoderForFormat(videoFormat)
+        if(decoderCodecName != null){
+            Log.i(TAG, "createMediaCodecDecoderVideo: decoderCodecName=$decoderCodecName")
+        }else{
+            Log.i(TAG, "createMediaCodecDecoderVideo: decoderCodecName=null")
+            return
+        }
+
+        // create decoder
+        val decoder = MediaCodec.createByCodecName(decoderCodecName)
+        decoder.configure(videoFormat, null, null, 0)
+
+        // ... decoding
+
+        // release
+        decoder.stop()
+        decoder.release()
+    }
+
+    @Test
+    fun createMediaCodecEncoder(){
+        val mime = "video/avc"
+        val width = 3840
+        val height = 2161
+        val videoFormat = MediaFormat.createVideoFormat(mime, width, height)
+
+        // find decoder name
+        val encoderCodecName = mRegularCodes.findEncoderForFormat(videoFormat)
+        if(encoderCodecName != null) {
+            Log.i(TAG, "createMediaCodecEncoder: encoderCodecName=$encoderCodecName")
+        }else{
+            Log.i(TAG, "createMediaCodecEncoder: encoderCodecName=null")
+            return
+        }
+
+        // create decoder
+        val encoder = MediaCodec.createByCodecName(encoderCodecName)
+        encoder.configure(videoFormat, null, null, 0)
+
+        // encoding...
+
+        // release
+        encoder.stop()
+        encoder.release()
     }
 }
