3bfdc0e86f6848e8aca8feb21bfd5a3190506a62
jiemojiemo <460706836@qq.com>
2024-05-10 21:34:09 +0800

    feat: DecodeEditPlay.kt 基本跑通

--- app/src/main/java/com/example/learnmediacodec/DecodeEditPlay.kt
+++ app/src/main/java/com/example/learnmediacodec/DecodeEditPlay.kt
@@ -17,8 +17,12 @@ import javax.microedition.khronos.opengles.GL10
 
 class DecodeEditPlay : AppCompatActivity(), SurfaceTexture.OnFrameAvailableListener {
     private val TAG = "DecodeEditPlay"
-    private var eglHelper: EGLHelper? = null
-    private var textureHandles = IntArray(1)
+    private var mOutputSurface: Surface? = null
+    private var mSurfaceTexture : SurfaceTexture? = null
+    private var mTextureRenderer: TextureRenderer2? = null
+    private var width = 0
+    private var height = 0
+    private val texMatrix = FloatArray(16)
     private val lock = Object()
 
     @Volatile
@@ -29,30 +33,13 @@ class DecodeEditPlay : AppCompatActivity(), SurfaceTexture.OnFrameAvailableListe
             gl: GL10?,
             config: javax.microedition.khronos.egl.EGLConfig?
         ) {
-            // create texture on gl thread
-            GLES20.glGenTextures(1, textureHandles, 0)
-            checkGlError("glGenTextures")
+            mTextureRenderer = TextureRenderer2()
+            Matrix.setIdentityM(texMatrix, 0)
 
-            // bind texture id to oes and config it
-            GLES20.glBindTexture(GLES11Ext.GL_TEXTURE_EXTERNAL_OES, textureHandles[0])
-            GLES20.glTexParameteri(
-                GLES11Ext.GL_TEXTURE_EXTERNAL_OES, GLES20.GL_TEXTURE_MIN_FILTER,
-                GLES20.GL_NEAREST
-            )
-            GLES20.glTexParameteri(
-                GLES11Ext.GL_TEXTURE_EXTERNAL_OES, GLES20.GL_TEXTURE_MAG_FILTER,
-                GLES20.GL_LINEAR
-            )
-            GLES20.glTexParameteri(
-                GLES11Ext.GL_TEXTURE_EXTERNAL_OES, GLES20.GL_TEXTURE_WRAP_S,
-                GLES20.GL_CLAMP_TO_EDGE
-            )
-            GLES20.glTexParameteri(
-                GLES11Ext.GL_TEXTURE_EXTERNAL_OES, GLES20.GL_TEXTURE_WRAP_T,
-                GLES20.GL_CLAMP_TO_EDGE
-            )
-            checkGlError("init oes texture")
-            Log.d(TAG, "config oes texture")
+            // create a thread to decode video
+            Thread {
+                decodeSync()
+            }.start()
         }
 
         override fun onSurfaceChanged(gl: GL10?, width: Int, height: Int) {
@@ -61,7 +48,25 @@ class DecodeEditPlay : AppCompatActivity(), SurfaceTexture.OnFrameAvailableListe
 
         override fun onDrawFrame(gl: GL10?) {
             Log.d(TAG, "onDrawFrame")
+            if(frameAvailable){
+                mSurfaceTexture?.updateTexImage()
+                mSurfaceTexture?.getTransformMatrix(texMatrix)
+            }
+
+            // draw oes texture to screen
+            mTextureRenderer?.draw(width, height, texMatrix, getMvp())
         }
+    }
+
+    private fun getMvp(): FloatArray {
+        val mvp = FloatArray(16)
+        Matrix.setIdentityM(mvp, 0)
+
+        // Set your transformations here
+        // Matrix.scaleM(mvp, 0, 1f, -1f, 1f)
+        //
+
+        return mvp
     }
 
     private var glRenderer = MyGLSurfaceRender()
@@ -80,24 +85,12 @@ class DecodeEditPlay : AppCompatActivity(), SurfaceTexture.OnFrameAvailableListe
 
     override fun onDestroy() {
         super.onDestroy()
-        eglHelper?.release()
-    }
-
-    fun checkGlError(op: String) {
-        var error: Int
-        while (GLES20.glGetError().also { error = it } != GLES20.GL_NO_ERROR) {
-            Log.e(
-                TAG,
-                "$op: glError $error"
-            )
-            throw java.lang.RuntimeException("$op: glError $error")
-        }
     }
 
     private fun decodeSync() {
-        val mSurfaceTexture = SurfaceTexture(textureHandles[0])
-        mSurfaceTexture.setOnFrameAvailableListener(this)
-        val mOutputSurface = Surface(mSurfaceTexture)
+        mSurfaceTexture = SurfaceTexture(mTextureRenderer!!.texId)
+        mSurfaceTexture!!.setOnFrameAvailableListener(this)
+        mOutputSurface = Surface(mSurfaceTexture)
 
         // create and configure media extractor
         val mediaExtractor = MediaExtractor()
@@ -107,6 +100,8 @@ class DecodeEditPlay : AppCompatActivity(), SurfaceTexture.OnFrameAvailableListe
         val videoTrackIndex = 0
         mediaExtractor.selectTrack(videoTrackIndex)
         val videoFormat = mediaExtractor.getTrackFormat(videoTrackIndex)
+        width = videoFormat.getInteger(MediaFormat.KEY_WIDTH)
+        height = videoFormat.getInteger(MediaFormat.KEY_HEIGHT)
 
         // create and configure media codec
         val codecList = MediaCodecList(MediaCodecList.REGULAR_CODECS)
@@ -156,6 +151,9 @@ class DecodeEditPlay : AppCompatActivity(), SurfaceTexture.OnFrameAvailableListe
                 if (info.size > 0) {
                     Log.i(TAG, "onOutputBufferAvailable")
                     codec.releaseOutputBuffer(outputBufferId, true)
+
+                    // sleep for 30ms to simulate 30fps
+                    Thread.sleep(30)
                 }
             }
 
@@ -182,6 +180,11 @@ class DecodeEditPlay : AppCompatActivity(), SurfaceTexture.OnFrameAvailableListe
         mediaExtractor.release()
         codec.stop()
         codec.release()
+
+        mOutputSurface!!.release()
+        mOutputSurface = null
+        mSurfaceTexture!!.release()
+        mSurfaceTexture = null
     }
 
     @SuppressLint("WrongConstant")
@@ -208,173 +211,6 @@ class DecodeEditPlay : AppCompatActivity(), SurfaceTexture.OnFrameAvailableListe
         synchronized(lock) {
             frameAvailable = true
             lock.notifyAll()
-        }
-    }
-
-    private fun waitTillFrameAvailable() {
-        synchronized(lock) {
-            while (!frameAvailable) {
-                lock.wait(500)
-                if (!frameAvailable)
-                    Log.e(TAG, "Surface frame wait timed out")
-            }
-            frameAvailable = false
-        }
-    }
-
-    inner class EGLHelper {
-        private val TAG = "EGLHelper"
-
-        private var mEGLDisplay = EGL14.EGL_NO_DISPLAY
-        private var mEGLSurface = EGL14.EGL_NO_SURFACE
-        private var mEGLContext: EGLContext? = null
-
-        fun setupEGL(width: Int, height: Int) {
-            mEGLDisplay = EGL14.eglGetDisplay(EGL14.EGL_DEFAULT_DISPLAY)
-            if (mEGLDisplay === EGL14.EGL_NO_DISPLAY) {
-                getError()
-                throw java.lang.RuntimeException("unable to get EGL14 display")
-            }
-            checkEglError("eglGetDisplay")
-
-            val version = IntArray(2)
-            if (!EGL14.eglInitialize(mEGLDisplay, version, 0, version, 1)) {
-                mEGLDisplay = null
-                getError()
-                throw java.lang.RuntimeException("unable to initialize EGL14")
-            }
-            checkEglError("eglInitialize")
-
-
-            // Configure EGL for pbuffer and OpenGL ES 2.0.  We want enough RGB bits
-            // to be able to tell if the frame is reasonable.
-            val attribList = intArrayOf(
-                EGL14.EGL_RED_SIZE, 8,
-                EGL14.EGL_GREEN_SIZE, 8,
-                EGL14.EGL_BLUE_SIZE, 8,
-                EGL14.EGL_RENDERABLE_TYPE, EGL14.EGL_OPENGL_ES2_BIT,
-                EGLExt.EGL_RECORDABLE_ANDROID, 1,
-                EGL14.EGL_NONE
-            )
-
-            val configs = arrayOfNulls<EGLConfig>(1)
-            val numConfigs = IntArray(1)
-            if (!EGL14.eglChooseConfig(
-                    mEGLDisplay,
-                    attribList,
-                    0,
-                    configs,
-                    0,
-                    configs.size,
-                    numConfigs,
-                    0
-                )
-            ) {
-                getError()
-                throw RuntimeException("eglChooseConfig failed")
-            }
-            checkEglError("eglCreateContext RGB888+recordable ES2")
-
-
-            // 创建 EGLContext
-            val contextAttrs = intArrayOf(
-                EGL14.EGL_CONTEXT_CLIENT_VERSION, 2,
-                EGL14.EGL_NONE
-            )
-            mEGLContext =
-                EGL14.eglCreateContext(
-                    mEGLDisplay,
-                    configs[0],
-                    EGL14.EGL_NO_CONTEXT,
-                    contextAttrs,
-                    0
-                )
-            if (mEGLContext == EGL14.EGL_NO_CONTEXT) {
-                getError()
-                throw RuntimeException("eglCreateContext failed")
-            }
-
-            // 创建 EGLSurface
-            val surfaceAttrib = intArrayOf(
-                EGL14.EGL_WIDTH, width,
-                EGL14.EGL_HEIGHT, height,
-                EGL14.EGL_NONE
-            )
-            mEGLSurface = EGL14.eglCreatePbufferSurface(mEGLDisplay, configs[0], surfaceAttrib, 0)
-            if (mEGLSurface == EGL14.EGL_NO_SURFACE) {
-                getError()
-                throw RuntimeException("eglCreateWindowSurface failed")
-            }
-        }
-
-        fun makeCurrent() {
-            if (!EGL14.eglMakeCurrent(mEGLDisplay, mEGLSurface, mEGLSurface, mEGLContext)) {
-                getError()
-                throw RuntimeException("eglMakeCurrent failed")
-            }
-        }
-
-        fun releaseEGL() {
-            if (!EGL14.eglMakeCurrent(
-                    mEGLDisplay,
-                    EGL14.EGL_NO_SURFACE,
-                    EGL14.EGL_NO_SURFACE,
-                    EGL14.EGL_NO_CONTEXT
-                )
-            ) {
-                throw java.lang.RuntimeException("eglMakeCurrent failed")
-            }
-        }
-
-        fun release() {
-            if (EGL14.eglGetCurrentContext() == mEGLContext) {
-                // Clear the current context and surface to ensure they are discarded immediately.
-                EGL14.eglMakeCurrent(
-                    mEGLDisplay, EGL14.EGL_NO_SURFACE, EGL14.EGL_NO_SURFACE,
-                    EGL14.EGL_NO_CONTEXT
-                )
-            }
-            EGL14.eglDestroySurface(mEGLDisplay, mEGLSurface)
-            EGL14.eglDestroyContext(mEGLDisplay, mEGLContext)
-            EGL14.eglTerminate(mEGLDisplay)
-
-            mEGLDisplay = null
-            mEGLContext = null
-            mEGLSurface = null
-        }
-
-        private fun checkEglError(msg: String) {
-            var failed = false
-            var error: Int
-            while (EGL14.eglGetError().also { error = it } != EGL14.EGL_SUCCESS) {
-                Log.e(TAG, msg + ": EGL error: 0x" + Integer.toHexString(error))
-                failed = true
-            }
-            if (failed) {
-                throw RuntimeException("EGL error encountered (see log)")
-            }
-        }
-
-        private fun getError() {
-            val errorCode = EGL14.eglGetError()
-            when (errorCode) {
-                EGL14.EGL_SUCCESS -> println("函数执行成功，无错误---没有错误")
-                EGL14.EGL_NOT_INITIALIZED -> println("对于特定的 Display, EGL 未初始化，或者不能初始化---没有初始化")
-                EGL14.EGL_BAD_ACCESS -> println("EGL 无法访问资源(如 Context 绑定在了其他线程)---访问失败")
-                EGL14.EGL_BAD_ALLOC -> println("对于请求的操作，EGL 分配资源失败---分配失败")
-                EGL14.EGL_BAD_ATTRIBUTE -> println("未知的属性，或者属性已失效---错误的属性")
-                EGL14.EGL_BAD_CONTEXT -> println("EGLContext(上下文) 错误或无效---错误的上下文")
-                EGL14.EGL_BAD_CONFIG -> println("EGLConfig(配置) 错误或无效---错误的配置")
-                EGL14.EGL_BAD_DISPLAY -> println("EGLDisplay(显示) 错误或无效---错误的显示设备对象")
-                EGL14.EGL_BAD_SURFACE -> println("未知的属性，或者属性已失效---错误的Surface对象")
-                EGL14.EGL_BAD_CURRENT_SURFACE -> println("窗口，缓冲和像素图(三种 Surface)的调用线程的 Surface 错误或无效---当前Surface对象错误")
-                EGL14.EGL_BAD_MATCH -> println("参数不符(如有效的 Context 申请缓冲，但缓冲不是有效的 Surface 提供)---无法匹配")
-                EGL14.EGL_BAD_PARAMETER -> println("错误的参数")
-                EGL14.EGL_BAD_NATIVE_PIXMAP -> println("NativePixmapType 对象未指向有效的本地像素图对象---错误的像素图")
-                EGL14.EGL_BAD_NATIVE_WINDOW -> println("NativeWindowType 对象未指向有效的本地窗口对象---错误的本地窗口对象")
-                EGL14.EGL_CONTEXT_LOST -> println("电源错误事件发生，Open GL重新初始化，上下文等状态重置---上下文丢失")
-                else -> {}
-            }
         }
     }
 }
